afa7d35a08e9b5e3da2457ce3abecd5db4b4d77e
guozeng <331323923@qq.com>
2018-08-31 00:25:47 +0800

    init express应用

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+// 中间件示例 其他router也会调用
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  console.log('中间件示例--------');
+  next();
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
