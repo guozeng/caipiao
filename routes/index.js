05cabcb701ce66affce4783f393fe68be7e6d444
guozeng <331323923@qq.com>
2018-08-31 00:26:27 +0800

    --

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 // 中间件示例 其他router也会调用
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
-  console.log('中间件示例--------');
   next();
 });
 
